0c5490fb8dee81b667d422e3da3a37839a48dc59
bakermd86 <bakermd6@gmail.com>
2023-03-29 10:53:37 +0200

    Setting Name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-rm -f TableRecords.ext
-zip TableRecords.ext -x build.sh -r ./*
-cp TableRecords.ext /mnt/c/Users/Michael/AppData/Roaming/SmiteWorks/Fantasy\ Grounds/extensions/
+rm -f CustomRecordTemplates.ext
+zip CustomRecordTemplates.ext -x build.sh -r ./*
+cp CustomRecordTemplates.ext /mnt/c/Users/Michael/AppData/Roaming/SmiteWorks/Fantasy\ Grounds/extensions/
 rm -f /mnt/c/Users/Michael/AppData/Roaming/SmiteWorks/Fantasy\ Grounds/cache/Test5/extensions/*
 rm -f /mnt/c/Users/Michael/AppData/Roaming/SmiteWorks/Fantasy\ Grounds/cache/Test6/extensions/*
 rm -f /mnt/c/Users/Michael/AppData/Roaming/SmiteWorks/Fantasy\ Grounds/cache/Starfinder\ Test/extensions/*
